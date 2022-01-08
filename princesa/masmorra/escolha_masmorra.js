f0ce3fd385f291b9cd0f6a709b4e238d1e63b3c0
Bruna Leardini Machado <bruna.leardininet@gmail.com>
2022-01-08 15:56:16 -0300

    Arrumando configurações no código

--- princesa/masmorra/escolha_masmorra.js
+++ princesa/masmorra/escolha_masmorra.js
@@ -1,18 +1,18 @@
 var escolhaMasmorra
 
-function escolhaMasmorra () {
-    escolhaMasmorra= prompt('1 - Aceita conversar;  2 - Não aceita conversar');
-while (true) {
-    if (escolhaMasmorra == 1 || escolhaMasmorra == 2) {
-        break
-    } else {
-        alert("Escolha inválida, digite 1 ou 2");
-        escolhaMasmorra= prompt('1 - Aceita conversar; 2 - Não aceita conversar');
+function escolhaMasmorra() {
+    escolhaMasmorra = prompt('1 - Aceita conversar;  2 - Não aceita conversar');
+    while (true) {
+        if (escolhaMasmorra == 1 || escolhaMasmorra == 2) {
+            break
+        } else {
+            alert("Escolha inválida, digite 1 ou 2");
+            escolhaMasmorra = prompt('1 - Aceita conversar; 2 - Não aceita conversar');
+        }
     }
-}
-if (escolhaMasmorra == 1) {
-    location = ('./convite_aceito.html')
-} else {
-    location = ('./convite_recusado.html')
-}
+    if (escolhaMasmorra == 1) {
+        location = ('./convite_aceito.html')
+    } else {
+        location = ('./convite_recusado.html')
+    }
 }
--- princesa/primeira_etapa/escolha_um.js
+++ princesa/primeira_etapa/escolha_um.js
@@ -1,18 +1,18 @@
 var escolhaUm
 
-function escolhaUm () {
-    escolhaUm= prompt('1 - Ir para casa nova 2 - Contar para todos');
-while (true) {
-    if (escolhaUm == 1 || escolhaUm == 2) {
-        break
-    } else {
-        alert("Escolha inválida, digite 1 ou 2");
-        escolhaUm = prompt('1 - Ir para casa nova 2 - Contar para todos');
+function escolhaUm() {
+    escolhaUm = prompt('1 - Ir para casa nova 2 - Contar para todos');
+    while (true) {
+        if (escolhaUm == 1 || escolhaUm == 2) {
+            break
+        } else {
+            alert("Escolha inválida, digite 1 ou 2");
+            escolhaUm = prompt('1 - Ir para casa nova 2 - Contar para todos');
+        }
     }
-}
-if (escolhaUm == 1) {
-    location = ('../segunda_etapa/casanova.html')
-} else {
-    location = ('../segunda_etapa/game_over_um.html')
-}
+    if (escolhaUm == 1) {
+        location = ('../segunda_etapa/casanova.html')
+    } else {
+        location = ('../segunda_etapa/game_over_um.html')
+    }
 }
